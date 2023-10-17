b4003be567d5152f293655fe3c1a970871f19e51
Chalaya22 <chalayaolga22@gmail.com>
2023-10-18 01:12:06 +0300

    comit 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoList from './TodoList';
+import todos from '../data/todos';
 
-export const App = () => {
-  return <div></div>;
-};
+export class App extends Component {
+  state = {
+    todos,
+  };
+  render() {
+    const { todos } = this.state;
+    return (
+      <div>
+        <h1>Coстоярие компонента</h1>
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
+}
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import css from './TodoList.module.css';
 
 const TodoList = ({ todos }) => {
-  return <ul> Создаем лист</ul>;
+  return (
+    <ul>
+      {todos.map(({ id, text }) => (
+        <li key={id} className={css.todoItems}>
+          <p className={css.todoText}>{text}</p>
+          <button type="button">Удалить</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 export default TodoList;
